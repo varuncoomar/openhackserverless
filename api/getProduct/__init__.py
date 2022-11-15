4f620de9d20b6dac86a47ee952dc327534b3a7c9
Varun Kumar <varuncoomar@gmail.com>
2022-11-16 00:09:18 +0800

    first commit

---
+++ api/getProduct/__init__.py
@@ -0,0 +1,24 @@
+import logging
+
+import azure.functions as func
+
+
+def main(req: func.HttpRequest) -> func.HttpResponse:
+    logging.info('Python HTTP trigger function getProduct processed a request.')
+
+    name = req.params.get('productId')
+    if not name:
+        try:
+            req_body = req.get_json()
+        except ValueError:
+            pass
+        else:
+            name = req_body.get('productId')
+
+    if name:
+        return func.HttpResponse(f"The product name for your product id {name} is Starfruit Explosion.")
+    else:
+        return func.HttpResponse(
+             "AVACADO, It is a scam fruit!!!.",
+             status_code=200
+        )
